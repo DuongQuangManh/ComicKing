ca2d32d3dc49c85970e5ae7fd51e5d9da9ea7f34
haijames32 <feckhai2003@gmail.com>
2023-11-03 15:00:37 +0700

    update

--- src/screens/User/ReadingHistory.tsx
+++ src/screens/User/ReadingHistory.tsx
@@ -3,6 +3,7 @@ import React, { useEffect, useState } from 'react'
 import { Screen } from '../screen'
 import { Header } from '@components'
 import { IReadingHistory } from '@models';
+import { WINDOW_WIDTH, helper, myColors } from '@utils';
 import { FlashList } from '@shopify/flash-list'
 import { useAppDispatch, useAppSelector } from '@redux/store';
 import { sendRequest } from '@api';
@@ -24,33 +25,46 @@ const ReadingHistory = () => {
     getListHistory();
   }, []);
 
-  const getListHistory = async ()  => {
+  const getListHistory = async () => {
     setState(pre => ({ ...pre, isLoading: true }))
     try {
       const res = await sendRequest('api/user/getHistoryReading', {
         userId: id,
       })
+      if (res.err == 200) {
+        setState(pre => ({
+          ...pre,
+          listHistory: res.data ?? [],
+          isLoading: false
+        }))
+      } else {
+        helper.showErrorMsg(res.message)
+        setState(pre => ({
+          ...pre,
+          isLoading: false
+        }))
+      }
     } catch (error) {
       console.log(error);
       setState(pre => ({
         ...pre,
-        isLoading: false,
+        isLoading: false
       }));
     }
   }
 
-// const dispatch = useAppDispatch();
-// const { historyReading = [] } = useAppSelector(state => state.userSlice);
+  // const dispatch = useAppDispatch();
+  // const { historyReading = [] } = useAppSelector(state => state.userSlice);
 
 
 
-return (
-  <Screen>
-    <Header text="History" />
+  return (
+    <Screen>
+      <Header text="History" />
 
 
-  </Screen>
-)
+    </Screen>
+  )
 }
 
 export default ReadingHistory
