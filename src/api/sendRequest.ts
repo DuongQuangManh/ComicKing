2f198aa2324c1438bbc1aab7ca9010fa12fa072e
DuongQuangManh <manhdqph20768@fpt.edu.vn>
2023-12-05 09:30:25 +0700

    change localhost

--- src/api/sendRequest.ts
+++ src/api/sendRequest.ts
@@ -10,7 +10,7 @@ export const sendRequest = (
   token = process.env.BASIC_TOKEN,
 ) => {
   // let url = `${process.env.HOST_NAME}${path}`;
-  let url = `${'http://192.168.1.3:1337/'}${path}`;
+  let url = `${'http://192.168.1.2:1337/'}${path}`;
   // let url = `${'http://192.168.32.106:1337/'}${path}`;
   return new Promise<ApiResult>(async function (resolve, reject) {
     let option = {
